6d7ab6f2138f6fc88989b4d2e0204c336efb22aa
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-10 13:57:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountTicket = 24657;
+        int oneMile = 20;
+
+        int bonusMile = amountTicket / oneMile;
+
+        System.out.println("Сумма начисленных миль за билет: " + bonusMile);
+    }
+}
