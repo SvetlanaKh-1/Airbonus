821933ee850ce1bfa9ba78a1fdc85600da1fce59
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-19 11:28:28 +0300

    Code optimization

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int amountTicket = 24657;
-        int oneMile = 20;
-
-        int bonusMile = amountTicket / oneMile;
-
+        int bonusMile = amountTicket / 20;
         System.out.println("Сумма начисленных миль за билет: " + bonusMile);
     }
 }
